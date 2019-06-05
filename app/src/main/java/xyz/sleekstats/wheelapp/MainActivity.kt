10808491a973666bc68181cb2b8c8c015d8b97ac
Eddie Foy <edwardfoyiii@gmail.com>
2019-06-05 21:38:07 +1000

    Implement spin animation for wheel
    Create a rotation animation for the wheel view, so when user clicks on
    the Spin button, the wheel is spun a random amount of degrees along its
    axis and stays in the position it stops in.
    Spin button is disabled while the wheel is still spinning, in order
    to prevent the animation from jumping around.
    The amount of degrees the wheel is randomly spun and time it is spun for
    will likely be adjusted later when more is implemented.

--- app/src/main/java/xyz/sleekstats/wheelapp/MainActivity.kt
+++ app/src/main/java/xyz/sleekstats/wheelapp/MainActivity.kt
@@ -3,22 +3,34 @@ package xyz.sleekstats.wheelapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.*
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
-
+/**This Activity displays a wheel of user-entered choices, along with a button that allows the user to spin the wheel.
+ * The wheel will eventually stop on one of the possible choices based on the random amount of degrees it's spun.
+**/
 class MainActivity : AppCompatActivity() {
 
     private val dummyColors = intArrayOf(Color.YELLOW, Color.RED, Color.BLUE, Color.GREEN)
+    private var wheelStartingDegrees = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val wheelBitMap : Bitmap = drawWheel(dummyColors)
+        val wheelBitMap: Bitmap = drawWheel(dummyColors)
         wheel_view.setImageBitmap(wheelBitMap)
+
+        spin_button.setOnClickListener {
+            val random = Random
+            spinWheel(random.nextInt(LARGEST_SPIN))
+        }
     }
 
-    private fun drawWheel(colors: IntArray) : Bitmap {
+    //Draw wheel based on width of screen and colors given
+    private fun drawWheel(colors: IntArray): Bitmap {
         val width = getWidthOfScreen()
         val bitmap = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888)
 
@@ -28,26 +40,58 @@ class MainActivity : AppCompatActivity() {
         val paint = Paint()
         paint.style = Paint.Style.FILL
 
-        var start = 0f
-        val amountOfColors = colors.size
+        var startAngle = 0f
+        val angleDegreesPerSection: Float = DEGREES_IN_CIRCLE / colors.size
 
         colors.forEach {
             paint.color = it
-            val angle: Float = DEGREES_IN_CIRCLE / amountOfColors
-            canvas.drawArc(rectF, start, angle, true, paint)
-            start += angle
+            canvas.drawArc(rectF, startAngle, angleDegreesPerSection, true, paint)
+            startAngle += angleDegreesPerSection
         }
         return bitmap
     }
 
-    private fun getWidthOfScreen() : Int {
+    private fun getWidthOfScreen(): Int {
         val display = windowManager.defaultDisplay
         val size = Point()
         display.getSize(size)
         return size.x
     }
 
+    //Rotate wheel the amount of degrees entered and update starting point
+    private fun spinWheel(degreesToSpinWheel: Int) {
+        val wheelEndingDegrees = wheelStartingDegrees + degreesToSpinWheel
+
+        val rotateAnimation = createRotateAnimation(wheelStartingDegrees, wheelEndingDegrees)
+        wheel_view.startAnimation(rotateAnimation)
+
+        wheelStartingDegrees = wheelEndingDegrees
+    }
+
+    //Create animation that rotates object around its center a certain amount of degrees and keeps it there
+    private fun createRotateAnimation(fromDegrees: Float, toDegrees: Float) : RotateAnimation {
+        val rotateAnimation = RotateAnimation(
+            fromDegrees, toDegrees,
+            Animation.RELATIVE_TO_SELF, .5f,
+            Animation.RELATIVE_TO_SELF, .5f
+        )
+        rotateAnimation.duration = SPIN_DURATION
+        rotateAnimation.fillAfter = true
+        rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                spin_button.isClickable = false
+            }
+            override fun onAnimationEnd(p0: Animation?) {
+                spin_button.isClickable = true
+            }
+            override fun onAnimationRepeat(p0: Animation?) {}
+        })
+        return rotateAnimation
+    }
+
     companion object {
-        const val DEGREES_IN_CIRCLE = 360.0f
+        const val DEGREES_IN_CIRCLE: Float = 360.0f
+        const val SPIN_DURATION: Long = 3000
+        const val LARGEST_SPIN = 1000
     }
 }
